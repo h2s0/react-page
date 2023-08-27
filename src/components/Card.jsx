2488449d1e0a182853b09ff691f43c23caf0bfa8
h2s0 <h2s0_@naver.com>
2023-08-28 01:29:07 +0900

    카드 컴포넌트 완ㅅ어

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,16 @@
-function Card({card}) {
+function Card({trip}) {
 
     return(
     <>
-    <div className="w-52 h-40 bg-blue-50 rounded-xl m-3 p-2">
-        <img src="{card.src}" alt="" className="w-full h-auto" />
-        <div className="">{card.title}</div>
-        <div className="flex items-center gap-1">
-            <img src="{card.country}" alt="" />
-            <div className="">{card.countryText}</div>
-            <div className="">{card.date}</div>
+    <div className="w-52 h-auto bg-blue-50 rounded-xl m-3 p-2">
+        <img src={trip.src} alt="여행사진" className="w-full h-auto" />
+        <div className="flex items-center gap-1 mt-4 justify-between">
+            <div className="text-sm">{trip.countryText}</div>
+            <span className="text-sm font-[300]">{trip.date}</span>
         </div>
     </div>
     </>
     )
-
 }
 
 export default Card
