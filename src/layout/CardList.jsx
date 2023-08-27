f07a12a3c7f29953677848a1ce2a06f844a150a4
h2s0 <h2s0_@naver.com>
2023-08-28 01:28:51 +0900

    데이터 배열 생성 및 반복문 작성

--- src/layout/CardList.jsx
+++ src/layout/CardList.jsx
@@ -1,19 +1,42 @@
-import Card from '../components/Card'
+import Card from '@/components/Card'
 
 const trips = [
     {
-
+        id:1,
+        src: '/imgs/Marseille.jpg',
+        countryText: 'France',
+        date: '2018.07'
     },
     {
-
+        id:2,
+        src: '/imgs/Cambridge.jpg',
+        countryText: 'United Kingdom',
+        date: '2020.10'
+    },
+    {
+        id:3,
+        src: '/imgs/Valencia.jpg',
+        countryText: 'Spain',
+        date: '2018.07'
+    },
+    {
+        id:4,
+        src: '/imgs/Interlaken.jpg',
+        countryText: 'Switzerland',
+        date: '2018.08'
     }
 ]
 
+
+
 function CardList() {
     return(
-        <div className='flex'>
-            <Card></Card>
-            <Card></Card>
+        <div className='flex flex-wrap justify-around'>
+            {trips.map(trip => (
+                <>
+                    <Card trip={trip} key={trips.id} />
+                </>
+            ))}
         </div>
     )
 }
