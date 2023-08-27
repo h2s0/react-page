9a97d57e0ea49ef585091bd7134afd08dcce3733
h2s0 <h2s0_@naver.com>
2023-08-27 23:22:16 +0900

    card.jsx 수정

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,11 +4,11 @@ function Card({card}) {
     <>
     <div className="w-52 h-40 bg-blue-50 rounded-xl m-3 p-2">
         <img src="{card.src}" alt="" className="w-full h-auto" />
-        <div className="">제목</div>
+        <div className="">{card.title}</div>
         <div className="flex items-center gap-1">
             <img src="{card.country}" alt="" />
-            <div className="">나라</div>
-            <div className="{card.date}">날짜</div>
+            <div className="">{card.countryText}</div>
+            <div className="">{card.date}</div>
         </div>
     </div>
     </>
