c5788cd700db2660cf35e658ad4125e37040a167
h2s0 <h2s0_@naver.com>
2023-08-28 16:31:39 +0900

    UI 수정

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,10 +6,13 @@ function Card({trip}) {
     return(
     <>
     <Link to={`/detail${trip.id}`}>
-        <div className="w-60 h-auto bg-slate-100 rounded-xl m-3 p-2">
+        <div className="w-64 h-auto bg-slate-100 rounded-xl m-3 p-3 hover:opacity-75">
             <img src={trip.src} alt="여행사진" className="w-full h-auto" />
             <div className="flex items-center gap-1 mt-4 justify-between">
-                <div className="text-sm">{trip.countryText}</div>
+                <div className='flex gap-2'>
+                    <img src={trip.countryImage} alt="" className='w-8 h-auto'/>
+                    <div className="text-sm">{trip.city}</div>
+                </div>
                 <span className="text-sm font-[300]">{trip.date}</span>
             </div>
         </div>
